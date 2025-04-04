c67c62d2852ac54450252926f6e555040d90a57b
franktb <frbajumu@gmail.com>
2025-04-04 15:09:01 +0100

    first

---
+++ main_window.py
@@ -0,0 +1,207 @@
+import sys
+import os
+
+from TreeWidget import TreeUtil
+from ui_main_window import Ui_MainWindow
+from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox, QInputDialog, QTreeWidget
+from PySide6.QtGui import QAction, QStandardItemModel, QStandardItem, QColor, QFont
+from PySide6.QtCore import Qt, QRunnable, QThreadPool, Slot, QObject, Signal
+import contextily as cx
+
+from matplotlib.backends.backend_qtagg import FigureCanvas
+from slippy_map_util import slippyMapNavigationToolbar
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
+
+from file_io.read_mag_data import MagCSV
+import pandas as pd
+import numpy as np
+
+import matplotlib.colors as colors
+
+from worker import Worker
+import time
+
+from util.gridding import grid
+from util.filter import running_mean_uniform_filter1d
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=3, dpi=150):
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        super().__init__(self.fig)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.project = None
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.actionNew_Project.triggered.connect(self.create_new_project)
+        self.ui.actionFrom_BOB_CSV.triggered.connect(self.select_BOB_CSV)
+        self.ui.actionFrom_Sealink_Folder.triggered.connect(self.select_SeaLINKFolder)
+
+        self.ui.actionDrawSelect.triggered.connect(self.draw_selection)
+        self.ui.actionRemoveOutlier.triggered.connect(self.remove_outlier)
+
+        self.mapping_2D_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        #self.mapping_2D_canvas = MplCanvas(self, 5,3,150)
+        self.ui.verticalLayout2DMappingCanvas.addWidget(slippyMapNavigationToolbar(self.mapping_2D_canvas, self))
+        self.ui.verticalLayout2DMappingCanvas.addWidget(self.mapping_2D_canvas)
+        self.mapping_2D_ax = self.mapping_2D_canvas.figure.subplots()
+
+        self.time_series_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        self.ui.verticalLayoutTimeSeriesCanvas.addWidget(NavigationToolbar(self.time_series_canvas))
+        self.ui.verticalLayoutTimeSeriesCanvas.addWidget(self.time_series_canvas)
+        self.time_series_ax = self.time_series_canvas.figure.subplots()
+
+        self.ui.pushButton.clicked.connect(self.debugTree)
+
+        self.TreeUtil = TreeUtil(self.ui.treeWidget)
+        self.magCSV = MagCSV()
+        #self.ui.treeWidget.setHeaderHidden(True)
+        # self.tree_model = QStandardItemModel()
+        # self.root_node = self.tree_model.invisibleRootItem()
+        # self.ui.treeView.setModel(self.project)
+
+        self.threadpool = QThreadPool()
+
+    def draw_selection(self):
+        checked_items = self.TreeUtil.checked_items()
+
+        survey_combined = pd.DataFrame()
+        for item in checked_items:
+            print(item.text(0))
+            survey_combined = pd.concat([survey_combined, item.data_frame])
+
+        survey_combined.astype({"Magnetic_Field": "float32"})
+        survey_combined = survey_combined.sort_values(by='datetime')
+        survey_combined.loc[survey_combined["Longitude"].astype(float) < -8.6, "Longitude"] = np.nan
+        survey_combined.loc[survey_combined["Magnetic_Field"].astype(float) < 45000., "Magnetic_Field"] = np.nan
+
+
+        survey_combined.ffill(inplace=True)
+
+        self.time_series_ax.plot(survey_combined["datetime"], survey_combined["Magnetic_Field"].astype(float))
+        self.time_series_canvas.draw_idle()
+
+        data_coordinates = np.array(
+            (survey_combined["Longitude"].astype(float), survey_combined["Latitude"].astype(float)))
+
+        x_min = -8.7
+        x_max = -7.3
+        y_min = 51.3
+        y_max = 51.99
+
+
+        #aspect = ((x_max - x_min) / 2000) / ((y_max - y_min) / 2000)
+
+        survey_combined.loc[:, "Magnetic_Field_Smoothed"] = running_mean_uniform_filter1d(
+            survey_combined.loc[:, "Magnetic_Field"].astype(float), 20)
+        survey_combined.loc[:, "Magnetic_Field_Ambient"] = running_mean_uniform_filter1d(
+            survey_combined.loc[:, "Magnetic_Field"].astype(float), 500)
+        survey_combined.loc[:, "Magnetic_Field_residual"] = survey_combined.loc[:,
+                                                         "Magnetic_Field_Smoothed"] - survey_combined.loc[:,
+                                                                                      "Magnetic_Field_Ambient"]
+
+        grid_x, grid_y, grid_z = grid(survey_combined["Magnetic_Field_residual"].astype(float), data_coordinates,
+                                      x_min,
+                                      x_max ,
+                                      2000j,
+                                      y_max,
+                                      y_min,
+                                      2000j, "linear")
+
+        #self.mapping_2D_ax.imshow(grid_z.T, origin='lower', extent=(x_min , x_max, y_min, y_max ))
+        self.mapping_2D_ax.set_xlim([x_min, x_max])
+        self.mapping_2D_ax.set_ylim([y_min, y_max])
+        cx.add_basemap(self.mapping_2D_ax, crs="EPSG:4326", source=cx.providers.OpenStreetMap.Mapnik)
+        #self.mapping_2D_ax.imshow(grid_z.T, origin='lower', extent=(x_min, x_max, y_min, y_max))
+
+        bounds = np.array([-200, -100, -50, -20, -10., -5, 0,5, 10, 20, 50, 100, 200])
+        norm = colors.BoundaryNorm(boundaries=bounds, ncolors=256)
+        #self.mapping_2D_ax.contourf(grid_x,grid_y,grid_z, origin='lower', extent=(x_min, x_max, y_min, y_max),
+        #           cmap='RdBu_r', )
+
+        self.contourf = self.mapping_2D_ax.contourf(grid_x, grid_y, grid_z, 200,origin='lower', extent=(x_min, x_max, y_min, y_max),
+                                    cmap='RdBu_r', norm= "symlog")
+
+        #self.mapping_2D_ax.contourf(grid_x,grid_y,grid_z, origin='lower', levels=10,
+        #                            norm=colors.SymLogNorm(linthresh=10, linscale=1,
+        #                                                   vmin=np.nanmin(grid_z), vmax=np.nanmax(grid_z), base=10))
+        self.mapping_2D_canvas.figure.colorbar(self.contourf ,ax = self.mapping_2D_ax, orientation="vertical" )
+        self.mapping_2D_canvas.draw_idle()
+
+
+
+
+    def debugTree(self):
+        print("hello")
+        # print(self.project.findItems("CV16_02_23042016.txt"))
+        print(self.project.findItems("CV16_02_23042016.txt").survey_frames[0])
+
+    def create_new_project(self):
+        project_name, ok = QInputDialog.getText(self, 'Input Dialog', 'Enter project name:')
+        if ok:
+            self.setWindowTitle(project_name)
+            # self.project = TreeModel(project_name)
+            self.ui.treeWidget = QTreeWidget(self.ui.centralwidget)
+        return ok
+
+    def select_BOB_CSV(self):
+        # if self.project != None:
+        selected_survey = QFileDialog.getOpenFileName(filter="All Files(*);;Text files(*.csv *.txt)")
+        if selected_survey[0].endswith((".txt", ".csv")):
+            worker = Worker(self.magCSV.read_from_BOBCSV, selected_survey[0],
+                            delimiter=",",
+                            skiprows=5,
+                            project=self.ui.treeWidget
+                            )
+
+            self.threadpool.start(worker)
+
+            # self.time_series_ax.plot(survey_frame.data_frame["GPS_Longitude"].astype(float),
+            #                     survey_frame.data_frame["GPS_Latitude"].astype(float), color="black")
+
+            # cx.add_basemap(self.mapping_2D_canvas.figure.get_axes()[0], crs="EPSG:4326", source=cx.providers.OpenStreetMap.Mapnik)
+            # self.mapping_2D_canvas.draw_idle()
+
+        else:
+            QMessageBox.critical(self, "File IO Error", "No text file selected!", )
+
+        # else:
+        #    ok = self.create_new_project()
+        #    if ok:
+        #        self.select_BOB_CSV()
+
+    def select_SeaLINKFolder(self):
+        selected_folder = QFileDialog.getExistingDirectory(parent=self,
+                                                           caption="Select directory",
+                                                           options=QFileDialog.Option.ShowDirsOnly)
+        print(selected_folder)
+        worker = Worker(self.magCSV.read_from_SeaLINKFolderXYZ, selected_folder,
+                        project=self.ui.treeWidget,
+                        )
+
+        self.threadpool.start(worker)
+
+        # else:
+        #    ok = self.create_new_project()
+        #    if ok:
+        #        self.select_SeaLINKFolder()
+
+
+
+    def remove_outlier(self):
+        return 0
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec())
