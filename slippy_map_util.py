1a5afed807e93ca348aa76074ad3e30af85989c4
franktb <frbajumu@gmail.com>
2025-05-08 13:20:03 +0100

    icon added and left aligned

--- slippy_map_util.py
+++ slippy_map_util.py
@@ -5,12 +5,26 @@ from PySide6.QtGui import QIcon
 class SlippyMapNavigationToolbar(NavigationToolbar):
     def __init__(self, canvas, parent=None):
         super(SlippyMapNavigationToolbar, self).__init__(canvas, parent)
-        self.custom_action = self.addAction(
-            QIcon.fromTheme("face-smile"),  # Use a theme icon or load your own with QIcon("path.png")
+
+        # Find the index of the stretchable spacer (usually last)
+        spacer_index = self._find_spacer_index()
+
+        self.lasso_action = self.addAction(
+            QIcon(r"./ui_elements/icons/cut-scissor-icon.png"),
+            #fromTheme("face-smile"),  # Use a theme icon or load your own with QIcon("path.png")
             "Custom Tool",
             self.select_lasso
         )
+        self.insertAction(self.actions()[spacer_index], self.lasso_action)
 
+    def _find_spacer_index(self):
+        # Typically the last action is the spacer that pushes widgets to the right
+        for i, action in enumerate(self.actions()):
+            if action.isSeparator():
+                continue
+            if action.iconText() == "":  # crude check for spacer
+                return i
+        return len(self.actions())
 
     def release_pan(self, *args):
         #EPSG:4326,EPSG:3857
